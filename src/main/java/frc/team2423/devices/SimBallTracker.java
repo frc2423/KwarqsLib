c5fc1755fdc687227f32f72dd2554ce70475a45f
Amory Galili <amorygalili@gmail.com>
2021-04-29 20:12:36 -0400

    Added ball tracker device code

--- src/main/java/frc/team2423/devices/SimBallTracker.java
+++ src/main/java/frc/team2423/devices/SimBallTracker.java
@@ -11,79 +11,66 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj.geometry.Transform2d;
 
-public class SimBallTracker extends Device implements IBallTracker{
-    
-        // Simulated Vision System.
+public class SimBallTracker extends Device implements IBallTracker {
+
+    // Simulated Vision System.
     // Configure these to match your PhotonVision Camera,
     // pipeline, and LED setup.
     PhotonCamera camera = new PhotonCamera("photonvision");
-    double camDiagFOV = 170.0; // degrees - assume wide-angle camera
-    double camPitch =  30; //Units.radiansToDegrees(Robot.CAMERA_PITCH_RADIANS); // degrees // tbd
-    double camHeightOffGround = .5; //Robot.CAMERA_HEIGHT_METERS; // meters // tbd
-    double maxLEDRange = 20; // meters
-    int camResolutionWidth = 640; // pixels
-    int camResolutionHeight = 480; // pixels
-    double minTargetArea = 10; // square pixels
+    private final double camDiagFOV = 170.0; // degrees - assume wide-angle camera
+    private final double maxLEDRange = 20; // meters
+    private final int camResolutionWidth = 640; // pixels
+    private final int camResolutionHeight = 480; // pixels
+    private final double minTargetArea = 10; // square pixels
+    private final double camPitchDegrees;
+    private final double camHeightOffGroundFeet;
 
-    SimVisionSystem simVision =
-            new SimVisionSystem(
-                    "photonvision",
-                    camDiagFOV,
-                    camPitch,
-                    new Transform2d(),
-                    camHeightOffGround,
-                    maxLEDRange,
-                    camResolutionWidth,
-                    camResolutionHeight,
-                    minTargetArea);
+    private final SimVisionSystem simVision;
 
-    public boolean hasTargets(){
+    public SimBallTracker(double camHeightOffGroundFeet, double camPitchDegrees) {
+        this.camHeightOffGroundFeet = Units.feetToMeters(camHeightOffGroundFeet);
+        this.camPitchDegrees = camPitchDegrees;
+        simVision = new SimVisionSystem("photonvision", camDiagFOV, camPitchDegrees, new Transform2d(),
+            Units.feetToMeters(camHeightOffGroundFeet), maxLEDRange, camResolutionWidth, camResolutionHeight,
+            minTargetArea);
+    }
+
+    public boolean hasTargets() {
         return camera.getLatestResult().hasTargets();
     }
 
-    public PhotonTrackedTarget getBestTarget(){
+    public PhotonTrackedTarget getBestTarget() {
         return camera.getLatestResult().getBestTarget();
     }
 
     public double getDistanceFromTarget() {
         if (hasTargets()) {
-            return PhotonUtils.calculateDistanceToTargetMeters(
-            camHeightOffGround,
-            0.2,
-            Units.degreesToRadians(camPitch),
-            Units.degreesToRadians(camera.getLatestResult().getBestTarget().getPitch()));
+            return PhotonUtils.calculateDistanceToTargetMeters(Units.feetToMeters(camHeightOffGroundFeet), 0.2,
+                    Units.degreesToRadians(camPitchDegrees),
+                    Units.degreesToRadians(camera.getLatestResult().getBestTarget().getPitch()));
         }
         return 0;
     }
 
-    public double getAngleFromTarget(){
+    public double getAngleFromTarget() {
         if (hasTargets()) {
             return camera.getLatestResult().getBestTarget().getYaw();
         }
         return 0;
     }
 
-    public void addSimulatedBall(double x, double y){
-        simVision.addSimVisionTarget(
-            new SimVisionTarget(new Pose2d(x, y, new Rotation2d()), 0.2, 0.2, 0.2));
+    public void addSimulatedBall(double x, double y) {
+        simVision.addSimVisionTarget(new SimVisionTarget(new Pose2d(x, y, new Rotation2d()), 0.2, 0.2, 0.2));
     }
 
-    public void giveRobotPose(Pose2d pose){
+    public void giveRobotPose(Pose2d pose) {
         simVision.processFrame(pose);
     }
 
-    public double getPitchFromTarget(){
+    public double getPitchFromTarget() {
         if (hasTargets()) {
             return camera.getLatestResult().getBestTarget().getPitch();
         }
         return 0;
-    }
-
-    @Override
-    public void report() {
-        reportValue("hasTargets", hasTargets());
-        reportValue("distanceFromTarget", getDistanceFromTarget());
-        reportValue("pitchFromTarget", getPitchFromTarget());
-        reportValue("angleFromTarget", getAngleFromTarget());
     }
 }
